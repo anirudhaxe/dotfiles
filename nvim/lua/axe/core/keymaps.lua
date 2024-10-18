57ec29a18ced76ab46e53f2f16fe795eafdcd499
Aniruddha Chaudhary <aniruddha@Aniruddhas-MacBook-Air.local>
2024-10-18 13:14:19 +0530

    feat!: removed bufferline tabing and added buffer support in lualine

--- nvim/lua/axe/core/keymaps.lua
+++ nvim/lua/axe/core/keymaps.lua
@@ -30,9 +30,14 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tl", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>th", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<leader>ba", "<cmd>Telescope buffers<CR>", { desc = "Show all open buffers in Telescope" })
+keymap.set("n", "<leader>bl", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
+keymap.set("n", "<leader>bh", "<cmd>bprev<CR>", { desc = "Go to previous buffer" })
+keymap.set("n", "<leader>bx", "<cmd>bd!<CR>", { desc = "Unload buffer and clear it from the buffer list" })
 
 -- Map Option+Left to move backward by word in insert mode
 keymap.set("i", "<ESC>b", "<C-o>b", { noremap = true, silent = true })
