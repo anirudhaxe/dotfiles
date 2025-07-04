4501d89ca1a2c11c92085e10adcd3888deceed96
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-07-04 23:56:54 +0530

    feat: removed yabai plugin

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -3,7 +3,7 @@
 FRONT_APP_SCRIPT='sketchybar --set $NAME label="$INFO"'
 
 yabai=(
-  script="$PLUGIN_DIR/yabai.sh"
+  # script="$PLUGIN_DIR/yabai.sh"
   icon.font="$FONT:Bold:16.0"
   label.drawing=off
   icon.width=30
