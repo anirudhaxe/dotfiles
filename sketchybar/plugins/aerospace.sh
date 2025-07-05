462b530422d0cbe5d82f7bf273d9cba5de63d0bd
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-07-05 11:39:15 +0530

    fix: fixed spaces section

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@ echo "called with $1"
 echo "$FOCUSED_WORKSPACE"
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME label.background.drawing=on
+    sketchybar --set $NAME background.drawing=on
 else
-    sketchybar --set $NAME label.background.drawing=off
+    sketchybar --set $NAME background.drawing=off
 fi
