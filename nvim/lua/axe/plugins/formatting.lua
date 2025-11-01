70ebe79902a6514327b56711c14d56271ed5c620
Aniruddha Chaudhary <axedotdev@outlook.com>
2025-11-01 13:15:51 +0530

    feat: added opencode config and prettier formatting

--- nvim/lua/axe/plugins/formatting.lua
+++ nvim/lua/axe/plugins/formatting.lua
@@ -5,6 +5,11 @@ return {
 		local conform = require("conform")
 
 		conform.setup({
+			formatters = {
+				prettier = {
+					prepend_args = { "--trailing-comma", "es5", "--tab-width", "2", "--print-width", "80" },
+				},
+			},
 			formatters_by_ft = {
 				javascript = { "prettier" },
 				typescript = { "prettier" },
