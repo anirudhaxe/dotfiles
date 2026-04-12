e1763828dbc58c395e27af59961a62dccc83cc8c
Aniruddha Chaudhary <code.anirudh.dev@gmail.com>
2026-04-13 01:01:31 +0530

    chore: changed config

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -27,11 +27,6 @@ return {
 				sql = { "pg_format" },
 				graphql = { "prettier" },
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
 		},
 		keys = {
 			{
